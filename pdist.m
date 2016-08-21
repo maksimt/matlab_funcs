47f860024f83a8eec061ed25901fde838bfc0add
maksimt <MaksimTsikhanovich@gmail.com>
2016-08-21 13:58:59 -0400

    Add files via upload

---
+++ pdist.m
@@ -0,0 +1,14 @@
+function [ D ] = pdist( X )
+%MYPDIST computes squared euclidean distance between each row of X
+%input:
+% X - n*d n vectors in d dimensions
+%output:
+% D - n*n D_ij = D_ji is norm(X(i,:)-X(j,:))^2
+
+[n,~] = size(X);
+nx = dot(X,X,2);
+N = repmat(nx,1,n);
+D = N+N'-2*(X*X');
+
+end
+
---
+++ pdist2.m
@@ -0,0 +1,20 @@
+function [ D ] = pdist2( X,Y )
+%PDIST2 computes the squared euclidean distance between rows of X and rows
+%of Y
+%input:
+% X - n*d n rows in d dimensions
+% Y - m*d m rows in d dimensions
+%output:
+% D - n*m D_ij = norm(X(i,:)-Y(j,:))
+
+[n,dx] = size(X);
+[m,dy] = size(Y);
+assert(dx==dy,'X and Y must have same column dimension');
+nx = dot(X,X,2);
+ny = dot(Y,Y,2);
+Nx = repmat(nx,1,m);
+Ny = repmat(ny,1,n);
+D = Nx + Ny' - 2*X*Y';
+
+end
+
